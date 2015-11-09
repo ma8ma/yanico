1f7c9cd7a412f7a2294c75ee8e2d509fde5699c5
Masayuki Yamamoto <ma3yuki.8mamo10@gmail.com>
2015-11-09 15:03:45 +0900

    New setup.py
    Insert version number from yanico module.
    Insert long description from README.rst.

---
+++ setup.py
@@ -0,0 +1,51 @@
+"""Setup script."""
+#  Copyright 2015 Masayuki Yamamoto
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+from setuptools import find_packages
+from setuptools import setup
+
+from yanico import __version__
+
+
+with open('README.rst', 'r', encoding='utf-8') as file:
+    README = file.read()
+
+setup(
+    name='yanico',
+    version=__version__,
+    author='Masayuki Yamamoto',
+    author_email='ma3yuki.8mamo10@gmail.com',
+    description='Yet Another Niconico-douga Command-line Interface',
+    long_description=README,
+    license='Apache 2.0',
+    keywords='cli comment niconico video',
+    url='https://github.com/ma8ma/yanico',
+    packages=find_packages(exclude=['tests']),
+    test_suite='tests',
+    install_requires=[],
+    entry_points={
+    },
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Environment :: Console',
+        'Intended Audience :: End Users/Desctop',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Internet :: WWW/HTTP :: Session',
+    ],
+)
