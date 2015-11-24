c77bc4be5879a4c3acad6aa14714c1f392be37a1
Masayuki Yamamoto <ma3yuki.8mamo10@gmail.com>
2015-11-24 22:19:44 +0900

    Add WWW/HTTP topic

--- setup.py
+++ setup.py
@@ -49,6 +49,7 @@ setup(
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Session',
     ],
 )
