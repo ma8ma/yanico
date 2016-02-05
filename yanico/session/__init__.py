98a833fe6f0d270eb1589a69ef653388395cf97d
Masayuki Yamamoto <ma3yuki.8mamo10@gmail.com>
2016-02-05 10:40:57 +0900

    Add UserSessionNotFoundError exception
    Raise when user_session is not found.

--- yanico/session/__init__.py
+++ yanico/session/__init__.py
@@ -12,3 +12,7 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
+
+
+class UserSessionNotFoundError(Exception):
+    """Firefox profile exists, buf user_session is not found."""
